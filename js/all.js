96cebf212e1d6b9f0e10e992fb8a997691651bfa
Chi <yochi080684@gmail.com>
2022-06-28 22:39:30 +0800

    使用 json-server 存放資料

--- js/all.js
+++ js/all.js
@@ -2,7 +2,13 @@ function loginCheck() {
   const email_empty = document.querySelector('#login-email').value === ''
   const password_empty = document.querySelector('#login-password').value === ''
   if(!email_empty && !password_empty) {
-    window.location.href='todo.html'
+    fetch(`https://todo-json-00824.herokuapp.com/users?email=${document.querySelector('#login-email').value}&password=${document.querySelector('#login-password').value}`,{
+      method: "GET",
+      redirect: 'follow'
+    })
+    .then(res => res.json())
+    .then(result=> Object.keys(result).length > 0 ? window.location.href=`todo.html?id=${result[0].id}&name=${result[0].name}` : alert("帳號密碼錯誤!"))
+    .catch(err => console.log(err))
   } else {
     if(email_empty) {
       document.querySelector('#login-email').classList.remove('mb-2')
@@ -22,7 +28,22 @@ function registerCheck() {
   const password_empty = document.querySelector('#register-password').value === ''
   const password_check_empty = document.querySelector('#register-password-check').value === ''
   if(!email_empty && !name_empty & !password_empty && !password_check_empty) {
-    window.location.href='todo.html'
+    const data = {
+      "email": document.querySelector('#register-email').value,
+      "name": document.querySelector('#register-name').value,
+      "password": document.querySelector('#register-password').value
+    }
+    fetch('https://todo-json-00824.herokuapp.com/users',{
+      method: "POST",
+      redirect: 'follow',
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(result=> window.location.href='index.html')
+    .catch(err => console.log(err))
   } else {
     if(email_empty) {
       document.querySelector('#register-email').classList.remove('mb-2')
--- js/todo.js
+++ js/todo.js
@@ -1,59 +1,43 @@
-let todo_list = [
-  {
-    content:'把冰箱發霉的檸檬拿去丟',
-    status: false
-  },
-  {
-    content:'打電話叫媽媽匯款給我',
-    status: true
-  },
-  {
-    content:'整理電腦資料夾',
-    status: false
-  },
-  {
-    content:'繳電費水費冷氣費',
-    status: true
-  },
-  {
-    content:'約vicky禮拜三泡溫泉',
-    status: false
-  },
-  {
-    content:'約ada禮拜四吃晚餐',
-    status: true
-  }
-]
+const url = new URL(window.location.href)
+const params = url.searchParams;
+const id = params.get("id")
+let todo_list = []
+document.querySelector('#whoseTodo').innerText = `${params.get("name")} 的 Todo`
+fetch(`https://todo-json-00824.herokuapp.com/todo?userID=${id}&_sort=id&_order=desc`, {
+  method: "GET",
+  redirect: 'follow'
+})
+  .then(res => res.json())
+  .then(result => {
+    todo_list = result
+    updateList()
+    const tabs = [...document.getElementsByClassName('tabs-item')]
+
+    tabs.forEach(item => {
+      item.addEventListener('click', () => {
+        if (!item.classList.contains('is-active')) {
+          document.querySelector('.is-active').classList.remove('is-active')
+          item.classList.add('is-active')
+          switch (item.textContent) {
+            case '全部':
+              current_tabs = item.textContent
+              break;
+            case '待完成':
+              current_tabs = item.textContent
+              break;
+            case '已完成':
+              current_tabs = item.textContent
+              break;
+          }
+          getList()
+        }
+      })
+    })
+  })
+  .catch(err => console.log(err))
 
 let show_list = [...todo_list]
 let current_tabs = '全部'
-
-window.onload = function () {
-  // 進入頁面先更新列表
-  updateList()
-  const tabs = [...document.getElementsByClassName('tabs-item')]
-
-  tabs.forEach(item => {
-    item.addEventListener('click', () => {
-      if (!item.classList.contains('is-active')){
-        document.querySelector('.is-active').classList.remove('is-active')
-        item.classList.add('is-active')
-        switch (item.textContent) {
-          case '全部':
-            current_tabs = item.textContent
-            break;
-          case '待完成':
-            current_tabs = item.textContent
-            break;
-          case '已完成':
-            current_tabs = item.textContent
-            break;
-        }
-        getList()
-      }
-    })
-  })
-}
 
 function getList() {
   const list_block = document.querySelector('.todo-list')
@@ -70,14 +54,14 @@ function getList() {
   }
 
   let inner = ''
-  show_list.forEach((item,index) => {
+  show_list.forEach((item, index) => {
     if (item.status) {
-      inner += `<li class="todo-item fw-4 p-relative" onclick="statusToggle(${index})">
-                  <span class="status-box mr-2 p-relative completed"></span><span class="todo-content">${item.content}</span><button class="del-btn p-absolute" onclick="deleteTodo(event,${index})"></button>
+      inner += `<li class="todo-item fw-4 p-relative" onclick="statusToggle(${index},${item.id})">
+                  <span class="status-box mr-2 p-relative completed"></span><span class="todo-content">${item.content}</span><button class="del-btn p-absolute" onclick="deleteTodo(event,${index},${item.id})"></button>
                 </li>`
     } else {
-      inner += `<li class="todo-item fw-4 p-relative"  onclick="statusToggle(${index})">
-                  <span class="status-box mr-2"></span><span class="todo-content">${item.content}</span><button class="del-btn p-absolute" onclick="deleteTodo(event,${index})"></button>
+      inner += `<li class="todo-item fw-4 p-relative"  onclick="statusToggle(${index},${item.id})">
+                  <span class="status-box mr-2"></span><span class="todo-content">${item.content}</span><button class="del-btn p-absolute" onclick="deleteTodo(event,${index},${item.id})"></button>
                 </li>`
     }
   })
@@ -91,17 +75,33 @@ function getCounter() {
 }
 
 function enterCheck() {
-  if(event.key === 'Enter') {
+  if (event.key === 'Enter') {
     addTodo()
   }
 }
 
 function addTodo() {
   const result = todo_list.indexOf(todo_list.find(item => item.content === document.getElementById('input').value))
-  console.log(result);
-  if (result < 0){
-    todo_list.unshift({content: document.getElementById('input').value, status: false})
-    if(todo_list.length === 1) {
+  if (result < 0) {
+    const newData = {
+      content: document.getElementById('input').value,
+      status: false
+    }
+    todo_list.unshift(newData)
+    newData.userID = id
+    fetch(`https://todo-json-00824.herokuapp.com/todo`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      redirect: 'follow',
+      body: JSON.stringify(newData)
+    })
+      .then(res => res.json())
+      .then(result => result)
+      .catch(err => console.log(err))
+
+    if (todo_list.length === 1) {
       toggleBlock()
     }
     updateList()
@@ -109,9 +109,19 @@ function addTodo() {
   }
 }
 
-function deleteTodo(e, index) {
+function deleteTodo(e, index, id) {
   const result = todo_list.indexOf(todo_list.find(item => item.content === show_list[index].content))
   todo_list.splice(result, 1)
+  fetch(`https://todo-json-00824.herokuapp.com/todo/${id}`, {
+    method: "Delete",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    redirect: 'follow'
+  })
+    .then(res => res.json())
+    .then(result => result)
+    .catch(err => console.log(err))
   if (todo_list.length < 1) {
     toggleBlock()
   } else {
@@ -127,6 +137,7 @@ function updateList() {
   getCounter()
 }
 
+// TODO: 全部清除，json-server 好像沒有提供一次 delete 多筆的做法，用 Promise.all 做?
 function clearCompleted() {
   todo_list = todo_list.filter(item => item.status === false)
   if (todo_list.length < 1) {
@@ -143,8 +154,19 @@ function toggleBlock() {
   empty_block.classList.toggle('d-none')
 }
 
-function statusToggle(index) {
+function statusToggle(index, id) {
   const result = todo_list.find(item => item.content === show_list[index].content)
   result.status = !result.status
+  fetch(`https://todo-json-00824.herokuapp.com/todo/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    redirect: 'follow',
+    body: JSON.stringify({ "status": result.status })
+  })
+    .then(res => res.json())
+    .then(result => result)
+    .catch(err => console.log(err))
   updateList()
 }
